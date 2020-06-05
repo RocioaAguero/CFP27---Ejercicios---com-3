38d3cc0a23b2e82d6bf80dfb6fb8bd9ff5ef21d7
RocioaAguero <65718124+RocioaAguero@users.noreply.github.com>
2020-06-05 12:35:03 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+#include "demogorgon.h"
+
+const int DEF_ANIMO_JUGADOR = 0;
+const char DEF_NIVEL_CONFIANZA_JUGADOR = 'w';
+const float DEF_FUERZA_PSIQUICA = -1.0F;
+
+int main()
+{
+    comenzar_enfrentamiento;
+    int recibir_animo();
+
+    return 0;
+}
