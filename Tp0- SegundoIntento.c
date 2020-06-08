6ea07edc161c1be77c57df1d5a937571954e9448
RocioaAguero <65718124+RocioaAguero@users.noreply.github.com>
2020-06-08 20:04:49 -0300

    Add files via upload

---
+++ Tp0- SegundoIntento.c
@@ -0,0 +1,88 @@
+/***JUGADOR***/
+#include <stdio.h>
+
+
+/*void comienza_enfrentamiento();
+    printf("Comienza el enfrentamiento");*/
+    
+
+const char CARACTER_NIVEL_BAJO='b';
+const char CARACTER_NIVEL_MEDIO='m';
+const char CARACTER_NIVEL_ALTO='a';
+
+const float NUMERO_CONFIANZA_BAJO=0.7;
+const float NUMERO_CONFIANZA_MEDIO=1.3;
+const float NUMERO_CONFIANZA_ALTO=3.7;
+
+
+
+int main(){
+    int valor_animo=-1;
+    char nivel_confianza='z';
+    int valor_nivel_confianza=-1;
+    float numero_nivel_confianza=0.0;
+    float fuerza_psiquica=0.0;
+    
+    printf("Comienza enfrentamiento \n");
+    
+    printf("Ingrese valor animo entre 1 y 100 \n");
+    scanf("%i", &valor_animo);
+    
+    if ((valor_animo>1) && (valor_animo<100)){
+        printf("Valor verificado\n");
+    }
+    else{
+        printf("Ingreso incorrecto vuelva a intentarlo\n");
+        scanf("%i", &valor_animo);
+    }
+    
+    printf("Ingrese valor confianza para BAJO= b, MEDIO= m, ALTO= a.\n");
+    scanf(" %c", &nivel_confianza);
+    
+    if (nivel_confianza=='a')
+    { printf("Confianza verificada: Alta \n");}
+    else if(nivel_confianza=='b')
+    { printf("Confianza recibida: Baja\n");}
+    else if (nivel_confianza=='m')
+    { printf("Confianza recibida: Media\n");}
+    else 
+    { printf("Ingreso incorrecto vuelva a intentarlo\n");
+        scanf(" %c", &nivel_confianza);
+    }
+    
+    if (nivel_confianza==CARACTER_NIVEL_ALTO){ 
+        printf("%f Valor asignado a confianza\n", NUMERO_CONFIANZA_ALTO);
+        
+    }else if (nivel_confianza==CARACTER_NIVEL_MEDIO){ 
+        printf("%f Valor asignado a confianza\n", NUMERO_CONFIANZA_MEDIO);
+        
+    }else if (nivel_confianza==CARACTER_NIVEL_BAJO){ 
+        printf("%f Valor asignado a confianza\n", NUMERO_CONFIANZA_BAJO);
+        
+    }else{ 
+        printf("ERROR\n");
+    }
+    
+    
+    /***
+    fuerza_psiquica==valor_animo*numero_nivel_confianza;
+    printf("%f Resultado fuerza psiquica \n", fuerza_psiquica);
+   
+    if (fuerza_psiquica<65){
+        printf ("Resultadoes desconocido\n");
+        printf ("jugador y contrincante desaparecen del enfrentamiento\n")
+        
+    }else if(fuerza_psiquica==65) && (fuerza_psiquica<67){
+        printf ("Resultado positivo\n");
+        printf ("Jugador ataque certero\n");
+        printf ("Jugador debilitado\n");
+        
+    }else if (fuerza_psiquica==67) && (fuerza_psiquica==370){
+        printf ("Resultado positivo\n");
+        printf ("Jugador vence contrincante\n");
+        
+    }
+    JUGADOR***/
+    
+return 0;
+}
